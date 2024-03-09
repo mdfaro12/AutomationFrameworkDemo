775d0e4f9fc94c484343b33736087d8991b5b4a6
91812 <91812@MDFARO>
2024-03-09 10:16:18 +0530

    First commit

---
+++ DemoAutomationFramework/src/test/java/practice/SampleTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void toLearnGitHub() {
+		Reporter.log("First push in GitHub",true);
+	}
+}
